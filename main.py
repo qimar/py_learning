67f6dda25d26fc7d94a08334d439d35801d33ff0
Ramiq Waqas Khan <ramiqwaqas@gmail.com>
2023-05-08 19:52:41 +0500

    My learning for loop

---
+++ main.py
@@ -0,0 +1,14 @@
+food = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]
+
+food [0] = "sushi"
+
+#food.append("ice cream")
+#food.remove("hamburger")
+#food.pop()
+#food.insert(0, "cake")
+#food.sort()
+#food.clear()
+
+for x in food:
+    print(x)
+    
