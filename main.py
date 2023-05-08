2dfcf5fe0cbaf1b3cac40c49c6b861c153503ecf
Ramiq Waqas Khan <ramiqwaqas@gmail.com>
2023-05-08 19:55:49 +0500

    arrays and loops

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ food = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]
 
 food [0] = "sushi"
 
-#food.append("ice cream")
+food.append("ice cream")
 #food.remove("hamburger")
 #food.pop()
-#food.insert(0, "cake")
+food.insert(0, "cake")
 #food.sort()
 #food.clear()
 
 for x in food:
     print(x)
-    
+     
