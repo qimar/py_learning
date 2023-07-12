e2929f36c8ce1a2f2bada20ffad11b745087d366
Ramiq Waqas Khan <ramiqwaqas@gmail.com>
2023-07-12 16:33:46 +0500

    Indexing

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
+name = "ramiq waqas!"
 
-#while True:
-    #name = input("What is your name? ")
-    #if name != "":
-        #pass
-    
-#while True:
-    #name = input("What is your name? ")
-    #if name != "":
-    #break
-    
-#while True:
-    #name = input("What is your name? ")
-    #if name != "":
-    #continue
- 
+first_name = name[0:5].upper()
+last_name = name[6:].upper()
+last_character = name[-1].upper()
+
+print(first_name)
+print(last_name)
+print(last_character)
